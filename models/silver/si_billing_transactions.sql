3cc111ff710458ff0f8be10ccd65a7f15a7ed22e
vkrishnaASC <vkrishna210103@gmail.com>
2025-12-22 21:00:15 +0000

    added files

--- models/silver/si_billing_transactions.sql
+++ models/silver/si_billing_transactions.sql
@@ -1,23 +1,28 @@
-{{ config(
-    materialized='table'
-) }}
 
-  with unique_transactions as (
+{{ config(materialized='table') }}
+
+-- 1. Identify rows that fail our 'Overcharge' business rule
+-- These will be inserted into your QUARANTINE_LOG manually or via a separate script
+-- For the demo, we simply filter them out of the main flow
+with quarantine_check as (
     select *,
-           -- We partition by everything EXCEPT the ID to find identical rows
+           case 
+             when plan_name = 'BASIC' and amount > 50 then 'Overcharge'
+             else null 
+           end as flag
+    from {{ ref('bz_billing_transactions') }}
+),
+
+unique_transactions as (
+    select * exclude flag,
            row_number() over (
                partition by customer_id, event_date, plan_name, amount 
                order by transaction_id asc
            ) as rn
-    from {{ ref('bz_billing_transactions') }}
+    from quarantine_check
+    where flag is null -- This keeps 'Bad' data out of Silver/Gold
 )
-select
-    transaction_id,
-    customer_id,
-    event_date,
-    event_type,
-    plan_name,
-    amount
+
+select transaction_id, customer_id, event_date, event_type, plan_name, amount
 from unique_transactions
-where rn = 1
-  and event_type != 'CANCEL'
+where rn = 1 and event_type != 'CANCEL'
