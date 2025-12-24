79ccbeaafb27f7d5d19e3ed6525ab47d5ec47dac
vkrishnaASC <vkrishna210103@gmail.com>
2025-12-24 05:19:33 +0000

    corrected fact

--- models/fact_customer_revenue.sql
+++ models/fact_customer_revenue.sql
@@ -4,6 +4,18 @@ with raw_data as (
     select * from {{ ref('raw_billing_transactions') }}
 ),
 
+-- Step 1: Clean & Filter (The "Good" data)
+cleaned_data as (
+    select 
+        transaction_id::int as transaction_id,
+        customer_id::varchar as customer_id,
+        event_date::date as event_date,
+        upper(plan_name) as plan_name,
+        amount::decimal(10,2) as amount
+    from raw_data
+)
+
+-- Step 2: Final Aggregation for the Client
 select 
     customer_id,
     max(plan_name) as current_plan,
@@ -12,5 +24,3 @@ select
     current_timestamp() as last_updated_at
 from cleaned_data
 group by 1
-
-
