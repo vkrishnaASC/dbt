6d81e47705c2bd7a0d74322a4aa0cabd29cccad1
vamsi krishna <akhilsunny946@gmail.com>
2026-01-08 15:25:47 +0000

    changed fct

--- models/fct_enrollments.sql
+++ models/fct_enrollments.sql
@@ -2,7 +2,6 @@ with calendar as (
     select * from {{ ref('dim_date') }}
 ),
 enrollment_data as (
-    -- Now includes course_id and credits
     select * from {{ ref('dim_enrollment_date') }}
 ),
 students as (
@@ -13,17 +12,24 @@ courses as (
 )
 
 select
-    c.enrollment_date,
+    -- We use 'cal' for calendar attributes
+    cal.enrollment_date,
+    cal.year,
+    cal.month_name,
+    cal.day_type,
+    
+    -- We use 'e' for transaction data
     e.student_id,
     e.course_id,
     coalesce(e.credits, 0) as credits,
+    
+    -- We use 's' for student attributes
     s.current_major,
-    c.course_category,
-    c.year,
-    c.month_name,
-    c.day_type
-from calendar c
-left join enrollment_data e on c.enrollment_date = e.enrollment_date
+    
+    -- We use 'crs' for course attributes
+    crs.course_category
+from calendar cal
+left join enrollment_data e on cal.enrollment_date = e.enrollment_date
 left join students s on e.student_id = s.student_id
-left join courses c on e.course_id = c.course_id
+left join courses crs on e.course_id = crs.course_id
 where e.student_id is not null
